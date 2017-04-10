053d647e85bb7874d41c3ff5d90399d36cb3da35
Olavi Haapala <ojhaapala@gmail.com>
2017-04-10 16:27:01 +0300

    Modify front page layout

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import FirstPage from './components/FirstPage';
 import HelpPage from './components/HelpPage';
 import RepairPage from './components/RepairPage';
-import './App.less';
+import './App.scss';
 
 const Home = () => <FirstPage />;
 const Help = () => <HelpPage />;
@@ -12,17 +12,21 @@ const Repair = () => <RepairPage />;
 
 const App = () => (
   <Router history={hashHistory}>
-    <div>
-      <Link to="/">Lindström logo here!</Link>
-
-      <Route exact path="/" component={Home} />
-      <Route path="/help" component={Help} />
-      <Route path="/repair" component={Repair} />
-
-      <ul>
-        <li><Link to="/help">Help</Link></li>
-        <li><Link to="/repair">Repair</Link></li>
-      </ul>
+    <div className="App">
+      <div className="header">
+        <Link className="logo" to="/" />
+      </div>
+      <div className="content">
+        <Route exact path="/" component={Home} />
+        <Route path="/help" component={Help} />
+        <Route path="/repair" component={Repair} />
+      </div>
+      <div className="footer">
+        <ul className="nav">
+          <li><Link to="/help">Help</Link></li>
+          <li><Link to="/repair">Repair</Link></li>
+        </ul>
+      </div>
     </div>
   </Router>
 );
