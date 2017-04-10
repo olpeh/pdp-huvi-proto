6fda0c7e73169fe81e3c5c7d16e5f21602e406c0
Olavi Haapala <ojhaapala@gmail.com>
2017-04-10 19:48:29 +0300

    Add initial version of the firstPage layout
    Arrow is not scaling nicely

--- src/components/FirstPage.js
+++ src/components/FirstPage.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import './FirstPage.scss';
 
 const FirstPage = () => (
-  <div>
-    <h2>Show ID to open</h2>
+  <div className="FirstPage">
+    <div className="FirstPage-wrapper">
+      <div className="arrow" />
+      <h2 className="FirstPage-heading">Show ID to open</h2>
+    </div>
   </div>
 );
 
