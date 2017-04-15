5f19a318410cdc9a82f2490ce332cc6553b17d1d
Olavi Haapala <ojhaapala@gmail.com>
2017-04-15 19:02:02 +0300

    Add ContactPage contents

--- src/pages/ContactPage.js
+++ src/pages/ContactPage.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './ContactPage.scss';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 
 const ContactPage = () => (
   <div className="ContactPage">
-    ContactPage
+    <div className="ContactPage-link-wrapper">
+      <a className="ContactPage-link" href="tel:12345678">
+        Call Lindström customer service
+      </a>
+      <a className="ContactPage-link" href="mailto:contact@lindstrom.fi">
+        Email Lindström customer service
+      </a>
+      <Link className="ContactPage-link" to="chat">
+        Chat with Lindström customer service
+      </Link>
+    </div>
     <Footer />
   </div>
 );
