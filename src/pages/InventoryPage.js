a6bdd282b6b0d7ea9bd085c95138a1805c579746
Antti Partanen <anttipartanen10@gmail.com>
2017-04-26 02:29:05 +0300

    ADD: Added the QR-code info to InventoryPage

--- src/pages/InventoryPage.js
+++ src/pages/InventoryPage.js
@@ -4,6 +4,14 @@ import Inventory from '../components/Inventory';
 
 const InventoryPage = () => (
   <div className="InventoryPage">
+
+    <div className="QR-code-info">
+      <div className="image" />
+      <div className="text">
+        You can scan QR-code on the clothing tag or search from the list below
+      </div>
+    </div>
+
     <Inventory />
   </div>
 );
