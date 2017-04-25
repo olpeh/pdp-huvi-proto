2f0d4e4149aae97d85c4c206a22862c68744ad13
Olavi Haapala <ojhaapala@gmail.com>
2017-04-25 20:39:47 +0300

    ADD: add initial version of laundryInfo

--- src/components/LaundryInfo.js
+++ src/components/LaundryInfo.js
@@ -2,17 +2,81 @@ import React from 'react';
 import moment from 'moment';
 import './LaundryInfo.scss';
 
-const LaundryInfo = () => (
-  <div className="LaundryInfo">
-    <h1 className="LaundryInfo-heading">My week</h1>
-    <span className="LaundryInfo-week-number">{moment().format('W')}</span>
-    <div className="LaundryInfo-date-info">
-      {moment().format('LLLL')}
+function renderWeekDayData(weekDay) {
+  if (!weekDay.data) {
+    return <div className="weekday-data" />;
+  }
+
+  return (
+    <div className="weekday-data">
+      <div className="weekday-data-icon jackets">
+        x{weekDay.data.jackets}
+      </div>
+      <div className="weekday-data-icon pants">
+        x{weekDay.data.pants}
+      </div>
+      <div className="weekday-data-icon shirts">
+        x{weekDay.data.shirts}
+      </div>
+      <div className="weekday-data-icon laundry" />
     </div>
-    <div className="LaundryInfo-visual-info">
-      TODO: LaundryInfo here!
+  );
+}
+
+const LaundryInfo = () => {
+  const weekData = [
+    {
+      weekDayData: [
+        {
+          day: 'Monday',
+          dayShort: 'Mon',
+        },
+        {
+          day: 'Tuesday',
+          dayShort: 'Tue',
+        },
+        {
+          day: 'Wednesday',
+          dayShort: 'Wed',
+        },
+        {
+          day: 'Thursday',
+          dayShort: 'Thu',
+          data: {
+            jackets: 1,
+            pants: 2,
+            shirts: 3,
+            laundryDay: true,
+          },
+        },
+        {
+          day: 'Friday',
+          dayShort: 'Fri',
+        },
+      ],
+    },
+  ];
+  return (
+    <div className="LaundryInfo">
+      <div className="LaundryInfo-heading">
+        New clean workwear will arrive on <b>Thursday</b>. Have a nice week!
+      </div>
+      <div className="LaundryInfo-date-info">
+        {moment().format('DD.MM.YYYY')}
+      </div>
+      <div className="LaundryInfo-week-calendar">
+        {weekData[0].weekDayData.map(weekDay => (
+          <div className="week-calendar-day" key={weekDay.day}>
+            {renderWeekDayData(weekDay)}
+            <div className="weekday-short">
+              {weekDay.dayShort}
+            </div>
+          </div>
+        ))}
+      </div>
+      <span className="LaundryInfo-week-number">{moment().format('W')}</span>
     </div>
-  </div>
-);
+  );
+};
 
 export default LaundryInfo;
