94d9231433241d820e247033a3dcf0904632360b
Antti Partanen <anttipartanen10@gmail.com>
2017-04-26 04:44:03 +0300

    FIX: Made the My workwear page look more like Janne's layout

--- src/components/Garment.js
+++ src/components/Garment.js
@@ -18,29 +18,57 @@ class Garment extends React.Component {
     const expanded = this.state.expanded
       ? 'garment-list expanded'
       : 'garment-list';
+
+    let toggle_message = "Show all";
+    if (this.state.expanded) {
+      toggle_message = "Minimize";
+    }
+
+    let garment_name_genitive = this.props.name;
+    if (garment_name_genitive.slice(-1) !== "s") {
+      garment_name_genitive += "s";
+    } else {
+      garment_name_genitive += "'";
+    }
+
     return (
       <div className="Garment">
-        <div className="stationary-info" onClick={() => this.expand()}>
+        <div className="stationary-info">
+          <div className="name">{this.props.name}</div>
 
+
+          <div className="columns">
+                    
           <div className="left">
-            <img
-              className="image"
-              src={this.props.img.src}
-              alt={this.props.img.alt}
-            />
+            <div className="icon" />
+          </div>
+
+          <div className="center">
+            <div className="size">Size</div>
+            <div className="amount">You have {this.state.instances.length}</div>
           </div>
 
           <div className="right">
-            <div className="name">
-              <strong>{this.props.name}</strong>
+            <div className="icon">
             </div>
-            <div className="size">
-              <strong>Model: </strong> {this.props.model}
-            </div>
-
-            <div className="detailed-information" />
           </div>
+
+
+        
         </div>
+
+        <div className="expand-toggle" onClick={() => this.expand()}>
+        
+        
+        <div className="text">{toggle_message} {garment_name_genitive}</div>
+        {!this.state.expanded && <div className="arrow-down"></div> }
+        {this.state.expanded && <div className="arrow-up"></div> }
+        </div>
+
+        </div>
+
+
+
         <div className={expanded}>
           {/*<div className="description">{this.props.description}</div>
           <div className="buttons">
