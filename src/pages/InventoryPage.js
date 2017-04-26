88840a710b8f0a13b957bfdb11cc4139a47f4c25
Antti Partanen <anttipartanen10@gmail.com>
2017-04-26 07:05:01 +0300

    FIX: Changed the image sizes from pixels to vw's

--- src/pages/InventoryPage.js
+++ src/pages/InventoryPage.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './InventoryPage.scss';
 import Inventory from '../components/Inventory';
+import { Link } from 'react-router-dom'; 
 
 const InventoryPage = () => (
   <div className="InventoryPage">
 
     <div className="QR-code-info">
-      <div className="image" />
+      <Link className="image" to="scan"/>
       <div className="text">
         You can scan QR-code on the clothing tag or search from the list below
       </div>
