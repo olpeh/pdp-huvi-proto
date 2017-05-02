b890a311cdfaed2ed3c9f32c8cae8e5386a8f694
Olavi Haapala <ojhaapala@gmail.com>
2017-05-02 21:28:52 +0300

    ADD: Make Android app that shows the UI in fullscreen webview

---
+++ android/Locker2.0/app/src/main/java/pdp/huvi/locker20/MainActivity.java
@@ -0,0 +1,20 @@
+package pdp.huvi.locker20;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        String url = "https://olpeh.github.io/pdp-huvi-proto/";
+        WebView webView = (WebView) findViewById(R.id.webview);
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webView.loadUrl(url);
+    }
+}
