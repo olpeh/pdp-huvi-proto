accb1e2312405d39e530eee2291a39029d535af0
Olavi Haapala <ojhaapala@gmail.com>
2017-05-11 23:01:39 +0300

    ADD: Add another week to the frontpage

--- src/components/LaundryInfo.js
+++ src/components/LaundryInfo.js
@@ -2,10 +2,10 @@ import React from 'react';
 import moment from 'moment';
 import './LaundryInfo.scss';
 
-function renderWeekdayShort(weekDay) {
+function renderWeekdayShort(weekDay, weekIndex) {
   return (
     <div className="weekday-short">
-      {weekDay.weekDay === moment().isoWeekday()
+      {weekDay.weekDay === moment().isoWeekday() && !weekIndex
         ? <div className="current">{weekDay.dayShort}</div>
         : weekDay.dayShort}
     </div>
@@ -113,6 +113,50 @@ const LaundryInfo = () => {
         },
       ],
     },
+    {
+      weekDayData: [
+        {
+          weekDay: weekdayNow,
+          day: weekdays[(weekdayNow - 1) % 5],
+          dayShort: weekdaysShort[(weekdayNow - 1) % 5],
+        },
+        {
+          weekDay: weekdayNow + 1,
+          day: weekdays[weekdayNow % 5],
+          dayShort: weekdaysShort[weekdayNow % 5],
+          data: {
+            jackets: {
+              amount: 1,
+              details: 'Alepa Softshell Jacket',
+            },
+            pants: {
+              amount: 2,
+              details: 'Alepa Trousers',
+            },
+            shirts: {
+              amount: 3,
+              details: 'Alepa T-Shirt',
+            },
+            laundryDay: true,
+          },
+        },
+        {
+          weekDay: weekdayNow + 2,
+          day: weekdays[(weekdayNow + 1) % 5],
+          dayShort: weekdaysShort[(weekdayNow + 1) % 5],
+        },
+        {
+          weekDay: weekdayNow + 3,
+          day: weekdays[(weekdayNow + 2) % 5],
+          dayShort: weekdaysShort[(weekdayNow + 2) % 5],
+        },
+        {
+          weekDay: weekdayNow + 4,
+          day: weekdays[(weekdayNow + 3) % 5],
+          dayShort: weekdaysShort[(weekdayNow + 3) % 5],
+        },
+      ],
+    },
   ];
 
   const remainingGarments = {
@@ -143,16 +187,25 @@ const LaundryInfo = () => {
       <div className="LaundryInfo-date-info">
         {moment().format('DD.MM.YYYY')}
       </div>
-      <div className="LaundryInfo-week-calendar">
-        {weekData[0].weekDayData.map(weekDay => {
-          const classes = `week-calendar-day ${weekDay.day}`;
-          return (
-            <div className={classes} key={weekDay.day}>
-              {renderWeekdayData(weekDay)}
-              {renderWeekdayShort(weekDay)}
-            </div>
-          );
-        })}
+      <div className="LaundryInfo-week-calendar-wrapper-wrapper">
+        <div className="LaundryInfo-week-calendar-wrapper">
+          {weekData.map((week, weekIndex) => {
+            const weekClasses = `LaundryInfo-week-calendar week-${weekIndex}`;
+            return (
+              <div className={weekClasses} key={weekIndex}>
+                {week.weekDayData.map(weekDay => {
+                  const classes = `week-calendar-day ${weekDay.day} week-${weekIndex}`;
+                  return (
+                    <div className={classes} key={weekDay.day}>
+                      {renderWeekdayData(weekDay)}
+                      {renderWeekdayShort(weekDay, weekIndex)}
+                    </div>
+                  );
+                })}
+              </div>
+            );
+          })}
+        </div>
       </div>
       <div className="LaundryInfo-details-wrapper">
         <div className="LaundryInfo-details">
