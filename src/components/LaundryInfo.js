90dc13f8686554af088550a93005ad8aec283d37
Olavi Haapala <ojhaapala@gmail.com>
2017-05-12 15:59:12 +0300

    ADD: Show weekends and more weeks

--- src/components/LaundryInfo.js
+++ src/components/LaundryInfo.js
@@ -2,187 +2,271 @@ import React from 'react';
 import moment from 'moment';
 import './LaundryInfo.scss';
 
-function renderWeekdayShort(weekDay, weekIndex) {
+function renderWeekdayShort(weekday, weekIndex) {
   return (
     <div className="weekday-short">
-      {weekDay.weekDay === moment().isoWeekday() && !weekIndex
-        ? <div className="current">{weekDay.dayShort}</div>
-        : weekDay.dayShort}
+      {weekday.weekday === moment().isoWeekday() && !weekIndex
+        ? <div className="current">{weekday.dayShort}</div>
+        : weekday.dayShort}
+      {isLaundryDay(weekday)
+        ? <div className="weekday-data-wrapper">
+            <span className="weekday-data-icon laundry" />
+          </div>
+        : null}
     </div>
   );
 }
 
-function renderWeekdayDataContents(
-  weekDay,
-  withDetails = false,
-  hideLaundry = false
-) {
+function renderWeekdayDataContents(data) {
+  const laundryDayData = data || {
+    jackets: {
+      amount: 1,
+      details: 'Alepa Softshell Jacket',
+    },
+    pants: {
+      amount: 2,
+      details: 'Alepa Trousers',
+    },
+    shirts: {
+      amount: 3,
+      details: 'Alepa T-Shirt',
+    },
+    laundryDay: true,
+  };
+
   return (
     <div className="weekday-data">
+      {data
+        ? null
+        : <div className="weekday-data-wrapper">
+            <div className="weekday-data-text arriving-garments">
+              Arriving garments
+            </div>
+          </div>}
       <div className="weekday-data-wrapper">
         <div className="weekday-data-icon jackets" />
         <div className="weekday-data-text">
-          x{weekDay.data.jackets.amount}
-        </div>
-        <div className="weekday-data-details">
-          {withDetails ? weekDay.data.jackets.details : null}
+          x{laundryDayData.jackets.amount}
         </div>
       </div>
       <div className="weekday-data-wrapper">
         <div className="weekday-data-icon pants" />
         <div className="weekday-data-text">
-          x{weekDay.data.pants.amount}
-        </div>
-        <div className="weekday-data-details">
-          {withDetails ? weekDay.data.pants.details : null}
+          x{laundryDayData.pants.amount}
         </div>
       </div>
       <div className="weekday-data-wrapper">
         <div className="weekday-data-icon shirts" />
         <div className="weekday-data-text">
-          x{weekDay.data.shirts.amount}
-        </div>
-        <div className="weekday-data-details">
-          {withDetails ? weekDay.data.shirts.details : null}
+          x{laundryDayData.shirts.amount}
         </div>
       </div>
-      {hideLaundry
-        ? null
-        : <div className="weekday-data-wrapper">
-            <div className="weekday-data-icon laundry" />
-          </div>}
     </div>
   );
 }
 
-function renderWeekdayData(weekDay) {
-  if (!weekDay.data) {
-    return <div className="weekday-data" />;
-  }
+function getLaundryDay() {
+  const weekdayNow = moment().isoWeekday();
+  const weekdays = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+    'Sunday',
+  ];
 
-  return renderWeekdayDataContents(weekDay);
+  const laundryDayIndex = weekdayNow >= 5 ? 0 : weekdayNow;
+  return weekdays[laundryDayIndex];
+}
+
+function isLaundryDay(weekday) {
+  const weekdayNow = moment().isoWeekday();
+  return (weekdayNow >= 5 && weekday.weekday === 1) ||
+    (weekday.weekday === weekdayNow + 1 && weekday.weekday <= 5);
+}
+
+function renderWeekdayData(weekday) {
+  if (isLaundryDay(weekday)) {
+    return renderWeekdayDataContents();
+  }
+  return <div className="weekday-data" />;
 }
 
 const LaundryInfo = () => {
   const weekdayNow = moment().isoWeekday();
-  const weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'];
-  const weekdaysShort = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'];
+  const weekdays = [
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Weekend',
+  ];
+  const weekdaysShort = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat/Sun'];
 
   const weekData = [
     {
-      weekDayData: [
+      weekdayData: [
         {
-          weekDay: weekdayNow,
-          day: weekdays[(weekdayNow - 1) % 5],
-          dayShort: weekdaysShort[(weekdayNow - 1) % 5],
+          weekday: weekdayNow % 6,
+          day: weekdays[(weekdayNow - 1) % 6],
+          dayShort: weekdaysShort[(weekdayNow - 1) % 6],
         },
         {
-          weekDay: weekdayNow + 1,
-          day: weekdays[weekdayNow % 5],
-          dayShort: weekdaysShort[weekdayNow % 5],
-          data: {
-            jackets: {
-              amount: 1,
-              details: 'Alepa Softshell Jacket',
-            },
-            pants: {
-              amount: 2,
-              details: 'Alepa Trousers',
-            },
-            shirts: {
-              amount: 3,
-              details: 'Alepa T-Shirt',
-            },
-            laundryDay: true,
-          },
+          weekday: (weekdayNow + 1) % 6,
+          day: weekdays[weekdayNow % 6],
+          dayShort: weekdaysShort[weekdayNow % 6],
         },
         {
-          weekDay: weekdayNow + 2,
-          day: weekdays[(weekdayNow + 1) % 5],
-          dayShort: weekdaysShort[(weekdayNow + 1) % 5],
+          weekday: (weekdayNow + 2) % 6,
+          day: weekdays[(weekdayNow + 1) % 6],
+          dayShort: weekdaysShort[(weekdayNow + 1) % 6],
         },
         {
-          weekDay: weekdayNow + 3,
-          day: weekdays[(weekdayNow + 2) % 5],
-          dayShort: weekdaysShort[(weekdayNow + 2) % 5],
+          weekday: (weekdayNow + 3) % 6,
+          day: weekdays[(weekdayNow + 2) % 6],
+          dayShort: weekdaysShort[(weekdayNow + 2) % 6],
         },
         {
-          weekDay: weekdayNow + 4,
-          day: weekdays[(weekdayNow + 3) % 5],
-          dayShort: weekdaysShort[(weekdayNow + 3) % 5],
+          weekday: (weekdayNow + 4) % 6,
+          day: weekdays[(weekdayNow + 3) % 6],
+          dayShort: weekdaysShort[(weekdayNow + 3) % 6],
+        },
+        {
+          weekday: (weekdayNow + 5) % 6,
+          day: weekdays[(weekdayNow + 4) % 6],
+          dayShort: weekdaysShort[(weekdayNow + 4) % 6],
         },
       ],
     },
     {
-      weekDayData: [
+      weekdayData: [
         {
-          weekDay: weekdayNow,
-          day: weekdays[(weekdayNow - 1) % 5],
-          dayShort: weekdaysShort[(weekdayNow - 1) % 5],
+          weekday: weekdayNow % 6,
+          day: weekdays[(weekdayNow - 1) % 6],
+          dayShort: weekdaysShort[(weekdayNow - 1) % 6],
         },
         {
-          weekDay: weekdayNow + 1,
-          day: weekdays[weekdayNow % 5],
-          dayShort: weekdaysShort[weekdayNow % 5],
-          data: {
-            jackets: {
-              amount: 1,
-              details: 'Alepa Softshell Jacket',
-            },
-            pants: {
-              amount: 2,
-              details: 'Alepa Trousers',
-            },
-            shirts: {
-              amount: 3,
-              details: 'Alepa T-Shirt',
-            },
-            laundryDay: true,
-          },
+          weekday: (weekdayNow + 1) % 6,
+          day: weekdays[weekdayNow % 6],
+          dayShort: weekdaysShort[weekdayNow % 6],
         },
         {
-          weekDay: weekdayNow + 2,
-          day: weekdays[(weekdayNow + 1) % 5],
-          dayShort: weekdaysShort[(weekdayNow + 1) % 5],
+          weekday: (weekdayNow + 2) % 6,
+          day: weekdays[(weekdayNow + 1) % 6],
+          dayShort: weekdaysShort[(weekdayNow + 1) % 6],
         },
         {
-          weekDay: weekdayNow + 3,
-          day: weekdays[(weekdayNow + 2) % 5],
-          dayShort: weekdaysShort[(weekdayNow + 2) % 5],
+          weekday: (weekdayNow + 3) % 6,
+          day: weekdays[(weekdayNow + 2) % 6],
+          dayShort: weekdaysShort[(weekdayNow + 2) % 6],
         },
         {
-          weekDay: weekdayNow + 4,
-          day: weekdays[(weekdayNow + 3) % 5],
-          dayShort: weekdaysShort[(weekdayNow + 3) % 5],
+          weekday: (weekdayNow + 4) % 6,
+          day: weekdays[(weekdayNow + 3) % 6],
+          dayShort: weekdaysShort[(weekdayNow + 3) % 6],
+        },
+        {
+          weekday: (weekdayNow + 5) % 6,
+          day: weekdays[(weekdayNow + 4) % 6],
+          dayShort: weekdaysShort[(weekdayNow + 4) % 6],
+        },
+      ],
+    },
+    {
+      weekdayData: [
+        {
+          weekday: weekdayNow % 6,
+          day: weekdays[(weekdayNow - 1) % 6],
+          dayShort: weekdaysShort[(weekdayNow - 1) % 6],
+        },
+        {
+          weekday: (weekdayNow + 1) % 6,
+          day: weekdays[weekdayNow % 6],
+          dayShort: weekdaysShort[weekdayNow % 6],
+        },
+        {
+          weekday: (weekdayNow + 2) % 6,
+          day: weekdays[(weekdayNow + 1) % 6],
+          dayShort: weekdaysShort[(weekdayNow + 1) % 6],
+        },
+        {
+          weekday: (weekdayNow + 3) % 6,
+          day: weekdays[(weekdayNow + 2) % 6],
+          dayShort: weekdaysShort[(weekdayNow + 2) % 6],
+        },
+        {
+          weekday: (weekdayNow + 4) % 6,
+          day: weekdays[(weekdayNow + 3) % 6],
+          dayShort: weekdaysShort[(weekdayNow + 3) % 6],
+        },
+        {
+          weekday: (weekdayNow + 5) % 6,
+          day: weekdays[(weekdayNow + 4) % 6],
+          dayShort: weekdaysShort[(weekdayNow + 4) % 6],
+        },
+      ],
+    },
+    {
+      weekdayData: [
+        {
+          weekday: weekdayNow % 6,
+          day: weekdays[(weekdayNow - 1) % 6],
+          dayShort: weekdaysShort[(weekdayNow - 1) % 6],
+        },
+        {
+          weekday: (weekdayNow + 1) % 6,
+          day: weekdays[weekdayNow % 6],
+          dayShort: weekdaysShort[weekdayNow % 6],
+        },
+        {
+          weekday: (weekdayNow + 2) % 6,
+          day: weekdays[(weekdayNow + 1) % 6],
+          dayShort: weekdaysShort[(weekdayNow + 1) % 6],
+        },
+        {
+          weekday: (weekdayNow + 3) % 6,
+          day: weekdays[(weekdayNow + 2) % 6],
+          dayShort: weekdaysShort[(weekdayNow + 2) % 6],
+        },
+        {
+          weekday: (weekdayNow + 4) % 6,
+          day: weekdays[(weekdayNow + 3) % 6],
+          dayShort: weekdaysShort[(weekdayNow + 3) % 6],
+        },
+        {
+          weekday: (weekdayNow + 5) % 6,
+          day: weekdays[(weekdayNow + 4) % 6],
+          dayShort: weekdaysShort[(weekdayNow + 4) % 6],
         },
       ],
     },
   ];
 
   const remainingGarments = {
-    weekDay: weekdayNow + 1,
-    day: weekdays[weekdayNow % 5],
-    dayShort: weekdaysShort[weekdayNow % 5],
-    data: {
-      jackets: {
-        amount: 1,
-        details: 'Alepa Softshell Jacket',
-      },
-      pants: {
-        amount: 1,
-        details: 'Alepa Trousers',
-      },
-      shirts: {
-        amount: 1,
-        details: 'Alepa T-Shirt',
-      },
+    jackets: {
+      amount: 1,
+      details: 'Alepa Softshell Jacket',
+    },
+    pants: {
+      amount: 1,
+      details: 'Alepa Trousers',
+    },
+    shirts: {
+      amount: 1,
+      details: 'Alepa T-Shirt',
     },
   };
 
   return (
     <div className="LaundryInfo">
       <div className="LaundryInfo-heading">
-        New clean workwear will arrive <b>tomorrow</b>. Have a nice workday!
+        New clean workwear will arrive on
+        {' '}
+        <b>{getLaundryDay()}</b>
+        . Have a nice workday!
       </div>
       <div className="LaundryInfo-date-info">
         {moment().format('DD.MM.YYYY')}
@@ -193,12 +277,12 @@ const LaundryInfo = () => {
             const weekClasses = `LaundryInfo-week-calendar week-${weekIndex}`;
             return (
               <div className={weekClasses} key={weekIndex}>
-                {week.weekDayData.map(weekDay => {
-                  const classes = `week-calendar-day ${weekDay.day} week-${weekIndex}`;
+                {week.weekdayData.map(weekday => {
+                  const classes = `week-calendar-day ${weekday.day} week week-${weekIndex}`;
                   return (
-                    <div className={classes} key={weekDay.day}>
-                      {renderWeekdayData(weekDay)}
-                      {renderWeekdayShort(weekDay, weekIndex)}
+                    <div className={classes} key={weekday.day}>
+                      {renderWeekdayData(weekday)}
+                      {renderWeekdayShort(weekday, weekIndex)}
                     </div>
                   );
                 })}
@@ -212,7 +296,7 @@ const LaundryInfo = () => {
           <div className="LaundryInfo-details-heading">
             Clean clothes remaining:
           </div>
-          {renderWeekdayDataContents(remainingGarments, false, true)}
+          {renderWeekdayDataContents(remainingGarments)}
         </div>
         <div className="LaundryInfo-notification">
           <div>
